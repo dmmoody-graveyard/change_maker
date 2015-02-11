e05f13bf7e2b9452f5c51b7f75cf2b1b1ac7645b
Duane M. Moody <dmmoody@gmail.com>
2015-02-11 09:44:01 -0800

    add quarter evaluation

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var coins = function(amount) {
+  var quarters = Math.floor(amount / .25);
+  return quarters;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
 describe('coins', function() {
   it('takes .25 and returns "quarter"', function() {
-    expect(coins(.25)).to.equal('quarter');
+    expect(coins(.25)).to.equal(1);
   });
 });
