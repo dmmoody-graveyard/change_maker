af08f3d28e5022e3462d6d0cc294c486f34e1f10
Duane M. Moody <dmmoody@gmail.com>
2015-02-11 12:05:21 -0800

    add lower starting denomination

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-var coins = function(amount) {
+var coins = function(amount, start) {
   amount *= 100
+  if(typeof(start)==='undefined') start = 'quarter';
   var change = {'quarter': 0, 'dime': 0, 'nickel': 0, 'penny': 0};
+  var denominations = ["quarter", "dime", "nickel", "penny"];
   var currency = {"quarter": 25, "dime": 10, "nickel": 5, "penny": 1};
-  for (var key in currency) {
+  start = denominations.indexOf(start)
+  for (var index = start; index < denominations.length; index ++) {
+    var key = denominations[index]
     while (amount >= currency[key]) {
       change[key] = Math.floor(amount / currency[key]);
       amount -= change[key] * currency[key]
--- spec/specs.js
+++ spec/specs.js
@@ -19,4 +19,24 @@ describe('coins', function() {
     expect(coins(.41)).to.eql({'quarter': 1, 'dime': 1, 'nickel': 1, 'penny': 1});
   });
 
+  it('takes 5.32 and returns quarters: 21, dimes: 0, nickels: 1, penny: 2', function() {
+    expect(coins(5.32)).to.eql({'quarter': 21, 'dime': 0, 'nickel': 1, 'penny': 2});
+  });
+
+  it('takes .60 and returns change object with dime equal to 6', function() {
+    expect(coins(.60, 'dime')).to.eql({'quarter': 0, 'dime': 6, 'nickel': 0, 'penny': 0});
+  });
+
+  it('takes .66 and returns quarters: 0, dimes: 6, nickels: 1, penny: 1', function() {
+    expect(coins(.66, 'dime')).to.eql({'quarter': 0, 'dime': 6, 'nickel': 1, 'penny': 1});
+  });
+
+  it('takes .66 and returns quarters: 0, dimes: 0, nickels: 12, penny: 1', function() {
+    expect(coins(.66, 'nickel')).to.eql({'quarter': 0, 'dime': 0, 'nickel': 13, 'penny': 1});
+  });
+
+  it('takes .66 and returns quarters: 0, dimes: 0, nickels: 0, penny: 66', function() {
+    expect(coins(.66, 'penny')).to.eql({'quarter': 0, 'dime': 0, 'nickel': 0, 'penny': 66});
+  });
+
 });
