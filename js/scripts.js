93a80d78522253dbb199545526603c68eb37b902
Duane M. Moody <dmmoody@gmail.com>
2015-02-11 10:52:56 -0800

    add dime spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 var coins = function(amount) {
-  var quarters = Math.floor(amount / .25);
-  return quarters;
-}
+  amount *= 100
+  var change = {'quarter': 0, 'dime': 0, 'nickel': 0, 'penny': 0};
+  var currency = {"quarter": 25, "dime": 10, "nickel": 5, "penny": 1};
+  for (var key in currency) {
+    while (amount >= currency[key]) {
+      change[key] = Math.floor(amount / currency[key]);
+      amount -= change[key] * currency[key]
+    };
+  };
+  return change;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,10 @@
 describe('coins', function() {
-  it('takes .25 and returns "quarter"', function() {
-    expect(coins(.25)).to.equal(1);
+  it('takes .25 and returns change object with quarter equal to 1', function() {
+    expect(coins(.25)).to.eql({'quarter': 1, 'dime': 0, 'nickel': 0, 'penny': 0});
   });
+  
+  it('takes .10 and returns change object with dime equal to 1', function() {
+    expect(coins(.10)).to.eql({'quarter': 0, 'dime': 1, 'nickel': 0, 'penny': 0});
+  });
+
 });
